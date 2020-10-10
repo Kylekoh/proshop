49e9c8a49ff425941b38a442a996df9d6b2c0aaa
kyle_koh <ykoh728@gmail.com>
2020-10-10 22:30:38 +0900

    add React-router-dom

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,35 +1,43 @@
-import React from "react";
+import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 import {
-  Navbar,
-  Nav,
-  Container,
-  NavDropdown,
-  Form,
-  FormControl,
-  Button,
-} from "react-bootstrap";
+	Navbar,
+	Nav,
+	Container,
+	NavDropdown,
+	Form,
+	FormControl,
+	Button,
+} from 'react-bootstrap';
+import { Link } from 'react-bootstrap/lib/Navbar';
 
 const Header = () => {
-  return (
-    <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-        <Container>
-          <Navbar.Brand href="/">ProShop</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-              <Nav.Link href="/cart">
-                <i className="fas fa-shopping-cart"></i> Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <i className="fas fa-user"></i>Sign In
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </header>
-  );
+	return (
+		<header>
+			<Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+				<Container>
+					<LinkContainer to="/">
+						<Navbar.Brand href="/">ProShop</Navbar.Brand>
+					</LinkContainer>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="ml-auto">
+							<LinkContainer to="/cart">
+								<Nav.Link>
+									<i className="fas fa-shopping-cart"></i> Cart
+								</Nav.Link>
+							</LinkContainer>
+							<LinkContainer to="/login">
+								<Nav.Link>
+									<i className="fas fa-user"></i>Sign In
+								</Nav.Link>
+							</LinkContainer>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</header>
+	);
 };
 
 export default Header;
