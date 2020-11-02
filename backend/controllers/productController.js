3980559195dcf48101e7347f49dd3a2900bd144c
kylekoh <ykoh728@gmail.com>
2020-11-02 09:56:43 +0900

    User authentication endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,7 +5,7 @@ import Product from "../models/productModel.js";
 // @route  GET /api/products
 // @access  Public
 
-const getProducts = asyncHandler((req, res) => {
+const getProducts = asyncHandler(async(req, res) => {
   const products = await Product.find({});
   res.json(products);
 });
