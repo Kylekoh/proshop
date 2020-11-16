211c0903c2b5151a4ee4a4d033297e94ce12b3ef
kylekoh <ykoh728@gmail.com>
2020-11-16 09:45:46 +0900

    set basic setting of ShippingScreen:

---
+++ frontend/src/screens/ShippingScreen.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+import { Form, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import FormContainer from "../components/FormContainer";
+
+const ShippingScreen = ({ history }) => {
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [country, setConutry] = useState("");
+  return <div>ShippingScreen</div>;
+};
+
+export default ShippingScreen;
